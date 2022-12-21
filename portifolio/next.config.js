68c80268cae98f3901b8cab6e108c318fb5bf05e
Elton Okawa <elton.okawa@gmail.com>
2022-12-21 11:11:01 -0300

    first commit

---
+++ portifolio/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    // allow ts files outside dir folder
+    // https://github.com/vercel/next.js/issues/9474
+    externalDir: true,
+  },
+}
+
+module.exports = nextConfig
