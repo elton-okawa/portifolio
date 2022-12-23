f54b13d8212e707319a545dae27de0fe9026622f
Elton Okawa <elton.okawa@gmail.com>
2022-12-23 14:59:30 -0300

    feat: add react eslint

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,8 +1,10 @@
 module.exports = {
-  plugins: ["@typescript-eslint"],
+  plugins: ["react", "@typescript-eslint"],
   extends: [
+    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier", // must be the last one
   ],
+  parser: "@typescript-eslint/parser",
   rules: {},
 };
