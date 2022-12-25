f0ef37e3209512bdf1b6a5b6910fad259d6979e6
Elton Okawa <elton.okawa@gmail.com>
2022-12-25 17:04:51 -0300

    feat: use navigation drawer

--- portifolio/pages/_app.tsx
+++ portifolio/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { NavigationDrawer, NavItem } from "@elton-okawa/navigation-drawer";
+import { Icon, mdiHome, mdiAccount } from "@elton-okawa/icons";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter();
+
+  return (
+    <>
+      <NavigationDrawer>
+        <NavItem
+          icon={<Icon icon={mdiHome} />}
+          label="Home"
+          selected={pathname === "/"}
+        />
+        <NavItem
+          icon={<Icon icon={mdiAccount} />}
+          label="Account"
+          selected={pathname === "/about"}
+        />
+      </NavigationDrawer>
+      <Component {...pageProps} />;
+    </>
+  );
 }
