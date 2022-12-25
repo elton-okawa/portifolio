394f2c91f517f6e68c5606d5fbdd75050bb8f85f
Elton Okawa <elton.okawa@gmail.com>
2022-12-25 17:37:47 -0300

    feat: add navigation on nav bar

--- portifolio/pages/_app.tsx
+++ portifolio/pages/_app.tsx
@@ -1,8 +1,31 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import { NavigationDrawer, NavItem } from "@elton-okawa/navigation-drawer";
 import { Icon, mdiHome, mdiAccount } from "@elton-okawa/icons";
+
+type ItemData = {
+  icon: string;
+  label: string;
+  path: string;
+};
+
+const navigation: { top: ItemData[]; bottom: ItemData[] } = {
+  top: [
+    {
+      icon: mdiHome,
+      label: "Home",
+      path: "/",
+    },
+    {
+      icon: mdiAccount,
+      label: "About",
+      path: "/about",
+    },
+  ],
+  bottom: [],
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
@@ -10,18 +33,22 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NavigationDrawer>
-        <NavItem
-          icon={<Icon icon={mdiHome} />}
-          label="Home"
-          selected={pathname === "/"}
-        />
-        <NavItem
-          icon={<Icon icon={mdiAccount} />}
-          label="Account"
-          selected={pathname === "/about"}
-        />
+        {navigation.top.map((data) => renderNavItem(pathname, data))}
       </NavigationDrawer>
       <Component {...pageProps} />;
     </>
+  );
+}
+
+function renderNavItem(pathname: string, data: ItemData) {
+  return (
+    <Link href={data.path}>
+      <NavItem
+        key={data.path}
+        icon={<Icon icon={data.icon} />}
+        label={data.label}
+        selected={pathname === data.path}
+      />
+    </Link>
   );
 }
