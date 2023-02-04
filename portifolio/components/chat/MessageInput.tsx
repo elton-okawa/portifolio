2fc18cf049d1da7ad3e607e448d58662f1af547f
Elton Okawa <elton.okawa@gmail.com>
2023-02-04 15:12:31 -0300

    feat: clear input after send

--- portifolio/components/chat/MessageInput.tsx
+++ portifolio/components/chat/MessageInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { IconButton } from '@elton-okawa/icon-button';
 import { SendIcon } from '@elton-okawa/icons';
@@ -7,23 +7,23 @@ import { Flex } from '@elton-okawa/flex';
 import styles from './styles.module.css';
 
 interface MessageInputProps {
-  onSubmit: (string) => void;
+  onSubmit: (string) => Promise<void>;
 }
 
 export default function MessageInput({ onSubmit }: MessageInputProps) {
+  const [message, setMessage] = useState('');
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    const form = event.target;
-    const formData = new FormData(form);
-
-    onSubmit(formData.get("message"));
+    onSubmit(message);
+    setMessage('');
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <Flex gap={1}>
-        <Input id="message" name="message" extraClasses={[styles.messageInput]}/>
+        <Input id="message" name="message" value={message} onChange={setMessage} extraClasses={[styles.messageInput]}/>
         <IconButton type='submit'>
           <SendIcon />
         </IconButton>
