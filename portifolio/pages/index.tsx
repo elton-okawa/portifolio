d7d29252e1f9ad06188680aa8ccc4cb60553aa4b
Elton Okawa <elton.okawa@gmail.com>
2023-04-23 16:50:38 -0300

    feat: add color emphasis

--- portifolio/pages/index.tsx
+++ portifolio/pages/index.tsx
@@ -48,34 +48,44 @@ function renderExperience() {
         alignItems="flex-start"
         justifyContent="center"
         extraClasses={[styles.experience]}
+        gap={1}
       >
         <Typography variant="h6">Hi, I'm</Typography>
         <span>
-          <Typography variant="h3" extraClasses={[styles.typewriter]}>
+          <Typography
+            variant="h2"
+            extraClasses={[styles.typewriter, styles.name]}
+          >
             Elton Yoshio Okawa
           </Typography>
         </span>
-        <Typography variant="h3" extraClasses={[styles.slideInUp]}>
-          Full Stack Software Developer with
-        </Typography>
-        <Typography
-          variant="h3"
-          extraClasses={[
-            styles.delayFirst,
-            styles.slideInUp,
-            styles.workingExperience,
-          ]}
-        >
-          + years of working experience
+        <span>
+          <Typography variant="h3" extraClasses={[styles.slideInUp]}>
+            Full Stack Software Developer with
+          </Typography>
+          <Typography
+            variant="h3"
+            extraClasses={[
+              styles.delayFirst,
+              styles.slideInUp,
+              styles.workingExperience,
+            ]}
+          >
+            + years of working experience
+          </Typography>
+        </span>
+        <br />
+        <Typography extraClasses={[styles.summary]}>
+          I like to write clean software code with automated tests that deliver
+          value and solve real world problems. I also believe that I can learn a
+          lot from my co-workers and help them to improve.
         </Typography>
         <Typography extraClasses={[styles.summary]}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
-          velit mi. Sed non massa urna. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas. Nullam mi
-          ante, pharetra porttitor dapibus at, cursus non nibh. Proin quam nisi,
-          ultrices non tortor non, hendrerit semper tortor. Aliquam vitae tempus
-          justo. Nam ultrices sapien et ipsum viverra cursus. Cras nunc enim,
-          ornare non molestie ut, malesuada at metus.
+          My values:{' '}
+          <span className={styles.emphasis}>
+            lifelong learning, empathy, continuous feedback and work-life
+            balance.
+          </span>
         </Typography>
       </Flex>
     </Container>
