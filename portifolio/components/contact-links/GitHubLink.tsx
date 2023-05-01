af0464b386cf69453aa737f0f1f026457b2e45ba
Elton Okawa <elton.okawa@gmail.com>
2023-05-01 10:16:29 -0300

    refactor: use constant

--- portifolio/components/contact-links/GitHubLink.tsx
+++ portifolio/components/contact-links/GitHubLink.tsx
@@ -3,10 +3,11 @@ import { GitHubIcon } from '@elton-okawa/icons';
 import { Link } from '@elton-okawa/link';
 
 import styles from './ContactLinks.module.css';
+import { GITHUB_URL } from 'lib/constants';
 
 export function GitHubLink() {
   return (
-    <Link href="https://github.com/elton-okawa">
+    <Link href={GITHUB_URL}>
       <GitHubIcon size="extraLarge" extraClasses={[styles.contact]} />
     </Link>
   );
--- portifolio/components/contact-links/LinkedinLink.tsx
+++ portifolio/components/contact-links/LinkedinLink.tsx
@@ -3,10 +3,11 @@ import { LinkedinIcon } from '@elton-okawa/icons';
 import { Link } from '@elton-okawa/link';
 
 import styles from './ContactLinks.module.css';
+import { LINKEDIN_URL } from 'lib/constants';
 
 export function LinkedinLink() {
   return (
-    <Link href="https://www.linkedin.com/in/elton-okawa/">
+    <Link href={LINKEDIN_URL}>
       <LinkedinIcon size="extraLarge" extraClasses={[styles.contact]} />
     </Link>
   );
